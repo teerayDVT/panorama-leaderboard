0586fb791020a3aeb283e12ef2e5dab145827d61
Taylor Ray <tray@jhb.dvt.co.za>
2021-05-18 18:13:42 +0200

    deleted api source files (can now be downloaded as an image from docker hub)

--- src/main/salary_slip/Config.java
+++
@@ -1,7 +0,0 @@
-package salary_slip;
-
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class Config {
-}
--- src/main/salary_slip/SalarySlipInterface.java
+++
@@ -1,7 +0,0 @@
-package salary_slip;
-
-public interface SalarySlipInterface {
-  Employee getEmployee();
-
-  String getEmployeeDetails();
-}
