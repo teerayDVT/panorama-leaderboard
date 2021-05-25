ab0a6ac6ead8591885112ef1b327617f485e09c6
Taylor Ray <tray@jhb.dvt.co.za>
2021-05-25 19:07:56 +0200

    added custom components (leaderboard + results). Added more dummy data

--- web/js/dummyData.js
+++ web/js/dummyData.js
@@ -11,7 +11,7 @@ export var teams = [
     },
     {
         position: 2,
-        team: "Celtic Queers",
+        teamName: "A",
         played: 17,
         won: 15,
         drawn: 1,
@@ -21,7 +21,7 @@ export var teams = [
     },
     {
         position: 18,
-        team: "Nic The Dick",
+        teamName: "B",
         played: 17,
         won: 5,
         drawn: 2,
@@ -30,3 +30,22 @@ export var teams = [
         points: 15
     },
     ];
+
+export var results = [
+    {
+        date: new Date(2021, 4, 19, 18, 30),
+        time: "18:30",
+        team1Name: "Skop n Dop",
+        team1Goals: 10,
+        team2Goals: 2,
+        team2Name: "Sparticans"
+    },
+    {
+        date: new Date(2021, 3, 19, 19, 15),
+        time: "19:15",
+        team1Name: "A",
+        team1Goals: 2,
+        team2Goals: 2,
+        team2Name: "B"
+    }
+]
