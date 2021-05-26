c34740e6badd28451bb492bc918ee8aa02a77c59
Taylor Ray <tray@jhb.dvt.co.za>
2021-05-26 17:53:23 +0200

    Updated styles for tables, split 'recent results' by date

--- web/js/dummyData.js
+++ web/js/dummyData.js
@@ -11,7 +11,7 @@ export var teams = [
     },
     {
         position: 2,
-        teamName: "A",
+        teamName: "Sparticans",
         played: 17,
         won: 15,
         drawn: 1,
@@ -21,7 +21,7 @@ export var teams = [
     },
     {
         position: 18,
-        teamName: "B",
+        teamName: "Celtics",
         played: 17,
         won: 5,
         drawn: 2,
@@ -29,11 +29,22 @@ export var teams = [
         goalDifference: -30,
         points: 15
     },
+    {
+        position: 22,
+        teamName: "United",
+        played: 17,
+        won: 1,
+        drawn: 6,
+        lost: 10,
+        goalDifference: -50,
+        points: 3
+    },
     ];
 
-export var results = [
+    date: new Date(2021, 4, 19)
+
+export var result = [
     {
-        date: new Date(2021, 4, 19, 18, 30),
         time: "18:30",
         team1Name: "Skop n Dop",
         team1Goals: 10,
@@ -41,7 +52,20 @@ export var results = [
         team2Name: "Sparticans"
     },
     {
-        date: new Date(2021, 3, 19, 19, 15),
+        time: "19:15",
+        team1Name: "A",
+        team1Goals: 2,
+        team2Goals: 2,
+        team2Name: "B"
+    },
+    {
+        time: "18:30",
+        team1Name: "Skop n Dop",
+        team1Goals: 10,
+        team2Goals: 2,
+        team2Name: "Sparticans"
+    },
+    {
         time: "19:15",
         team1Name: "A",
         team1Goals: 2,
